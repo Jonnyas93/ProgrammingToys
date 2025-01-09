fa12dcada242df2e70359cae5e25c0c6c3fa2df7
Jonathon Saxton <jonathon.a.saxton@gmail.com>
2025-01-09 18:35:53 +1100

    Finished Farey Sequnce

--- Edabit Challenges/FareySequence.py
+++ Edabit Challenges/FareySequence.py
@@ -1,12 +1,16 @@
 def farey(n):
     result = {}
     denominator = n
-    result[0/denominator] = "0/" + str(denominator)
-    while denominator >= 1:
-        for i in range(1, n):
+    result[0/denominator] = "0/1"
+    result[1] = "1/1"
+    while denominator > 1:
+        for i in range(1, denominator):
+            
             if denominator%i == 0:
-                result[(i/denominator)]=(str(i/i) + "/" + str(denominator/i))
+                #print("Simplify: "+str(i/i) + "/" + str(denominator/i))
+                result[(i/denominator)]=(str(int(i/i)) + "/" + str(int(denominator/i)))
             else:
+                #print("No simplify: "+str(i) + "/" + str(denominator))
                 result[(i/denominator)]=(str(i) + "/" + str(denominator))
         denominator -= 1
     sortedResult = [value for key, value in sorted(result.items())]
